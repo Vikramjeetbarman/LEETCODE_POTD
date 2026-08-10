623ee82decbf6005b926535c7434b51f94dd4334
Vikramjeet Barman <vikramjeetbarman123@gmail.com>
2026-08-10 21:52:04 +0530

    Time: 33 ms (74.74%), Space: 9 MB (82.55%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool winnerSquareGame(int n) {
         vector<bool> t(n+1,false);
-        t[0]=false;
         for(int i=1;i<n+1;i++){
             for(int k=1;k*k<=i;k++){
                 if(t[i-(k*k)]==false){
