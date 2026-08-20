77b8443da816b1485f3f0322e1b43fe3ce3468ce
Vikramjeet Barman <vikramjeetbarman123@gmail.com>
2026-08-20 23:44:45 +0530

    Time: 0 ms (100%), Space: 11.3 MB (55.72%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -4,10 +4,13 @@ public:
         int count=0;
         int n=arr.size();
         for(int i=0;i<n;i++){
-
-            if(arr[i]>arr[(i+1)%n]){
+            if(i==n-1){
+                if(arr[n-1]>arr[0]) count++;
+            }
+            else if(arr[i]>arr[i+1]){
             count++;
             }
+            
             if(count>1 ) return false;
         }
         return true;
