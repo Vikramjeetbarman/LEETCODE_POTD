30b52ff5951e42bdd816203bb45ec57ba65a4b7f
Vikramjeet Barman <vikramjeetbarman123@gmail.com>
2026-08-21 23:48:18 +0530

    Time: 170 ms (48.06%), Space: 20.7 MB (46.51%) - LeetHub

--- 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.cpp
+++ 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     typedef long long ll;
-    ll countelementleft(int mid,vector<int>& coins){
+    ll countelementleft(ll mid,vector<int>& coins){
            ll n=coins.size();
            ll countnum=0;
            for(int expression=1;expression<=(1<<n)-1;expression++){
             ll lcm=0;
-                 int order=0;
+                 ll order=0;
                  for(int i=0;i<n;i++){
                         if(expression & 1<<i) {
                             order++;
@@ -27,13 +27,12 @@ public:
            }
            return countnum;
     }
-    long long findKthSmallest(vector<int>& coins, int k) {
+    long long findKthSmallest(vector<int>& coins, ll k) {
         ll result=-1;
         ll l=1;
-        ll r= (ll)(*max_element(coins.begin(),coins.end())*k);
-        ll mid=0;
+        ll r= (ll)(*max_element(coins.begin(),coins.end()))*k;
         while(l<=r){
-            mid=l+(r-l)/2;
+            ll mid=l+(r-l)/2;
             if(countelementleft(mid,coins)>=k){
                 result=mid;
                 r=mid-1;
