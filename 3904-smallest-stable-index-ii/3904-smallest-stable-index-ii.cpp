7c700bcafad864f75c5a69cb5d52ddae120488c6
Vikramjeet Barman <vikramjeetbarman123@gmail.com>
2026-09-05 21:49:27 +0530

    Time: 11 ms (57.63%), Space: 210.8 MB (56.49%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -2,30 +2,21 @@ class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n=nums.size();
-        vector<int> prefmax(n);
-        vector<int> suffmin(n);
-        prefmax[0]=nums[0];
-        int max=prefmax[0];
-        suffmin[n-1]=nums[n-1];
-        int min=nums[n-1];
+        vector<int>suf(n);
+        vector<int>pref(n);
+        pref[0]=nums[0];
+        suf[n-1]=nums[n-1];
         for(int i=1;i<n;i++){
-            if(nums[i]>=max){
-                 max=nums[i];
-                 prefmax[i]=nums[i];
-            }
-            else prefmax[i]=max;
+            pref[i]=max(nums[i],pref[i-1]);
         }
         for(int i=n-2;i>=0;i--){
-            if(nums[i]<=min){
-                 min=nums[i];
-                 suffmin[i]=nums[i];
-            }
-            else suffmin[i]=min;
+            suf[i]=min(suf[i+1],nums[i]);
         }
+
         for(int i=0;i<n;i++){
-            if((prefmax[i]-suffmin[i])<=k) return i;
+            int temp=pref[i]-suf[i];
+            if(temp<=k)return i;
         }
         return -1;
-       
     }
 };
