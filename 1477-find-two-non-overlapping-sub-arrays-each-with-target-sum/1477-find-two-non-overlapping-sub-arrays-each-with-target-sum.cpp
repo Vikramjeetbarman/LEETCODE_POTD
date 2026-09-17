41f7b44808f41de7ba4ada1457fe5dabc0862724
Vikramjeet Barman <vikramjeetbarman123@gmail.com>
2026-09-17 23:25:47 +0530

    Time: 9 ms (65.98%), Space: 89 MB (67.53%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -4,26 +4,27 @@ public:
         int n=arr.size();
         int i=0;
         int j=0;
-         vector<int> minbestlentillidx(n,INT_MAX);
-         int currsum=0;
-         int result=INT_MAX;
-         int bestminlen=INT_MAX;
-         while(j<n){
-                currsum+=arr[j];
-                while(i<j && currsum>target){
-                    currsum -=arr[i++];
-                }
-                if(currsum==target){
-                          int length=j-i+1;
-                          if(i>0 && minbestlentillidx[i-1]!=INT_MAX){
-                            result=min(result,length+minbestlentillidx[i-1]);
-                          }
-                          bestminlen=min(bestminlen,length);
-                }
-                minbestlentillidx[j]=bestminlen;
-                j++;
+        int currsum=0;
+        int result=INT_MAX;
+        int bestminidx=INT_MAX;
+        vector<int> bestminidxtill(n,INT_MAX);
+        while(j<n){
+            currsum+=arr[j];
+            while(i<j && currsum> target){
+                currsum -= arr[i++];
 
-         }
-         return result==INT_MAX ? -1 :result ;
+            }
+            if(currsum==target){
+                int len=j-i+1;
+                if(i>0 && bestminidxtill[i-1]!=INT_MAX){
+                  result=min(result,len+bestminidxtill[i-1]);
+                }
+                  bestminidx=min(bestminidx,len);
+                
+            }
+            bestminidxtill[j]=bestminidx;
+            j++;
+        }
+        return result== INT_MAX ? -1 : result;
     }
 };
