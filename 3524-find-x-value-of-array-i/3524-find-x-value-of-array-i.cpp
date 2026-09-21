95b2843e3df742ee4c82209c26688d4d6e5665fe
Vikramjeet Barman <vikramjeetbarman123@gmail.com>
2026-09-21 20:56:25 +0530

    Time: 166 ms (42.86%), Space: 151.1 MB (70.54%) - LeetHub

--- 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.cpp
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     vector<long long> resultArray(vector<int>& nums, int k) {
-        vector<long long> result(k),state(k);
-        for (int value:nums) {
-            int rem =value%k;
-            vector<long long> next(k);
-            for (int r=0; r<k;r++) {
-                int productRem=(r*rem)%k;
-                next[productRem]+=state[r];
-                result[productRem]+=state[r];
+        int n=nums.size();
+        vector<long long> result(k,0);
+        vector<long long> prevCount(k,0);
+        for(int i=0;i<n;i++){
+            vector<long long> currCount(k,0);
+            int currElementRemainder = nums[i]%k;
+            currCount[currElementRemainder]++;
+            for(int oldRem = 0; oldRem <= k-1; oldRem++){
+                int newRemain = ((long long)oldRem * nums[i]%k) % k;
+
+                currCount[newRemain]+=prevCount[oldRem];
             }
-            next[rem]++;
-            result[rem]++;
-            state=next;
+            prevCount=move(currCount);
+            for(int x=0; x<=k-1;x++) {
+                result[x]+=prevCount[x];
+            }
         }
         return result;
     }
